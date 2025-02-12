e0d3c554576e677a3f0d6ab572811e39b8b52336
Demetre <demetre.de.alwis@gmail.com>
2025-02-12 17:52:03 +1100

    added sign in and tests page

---
+++ app/tests.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function SignInScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Tests go here</Text>
+    </View>
+  );
+}
